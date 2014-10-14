8476d4086902ad5411437a377a4a72aa260767f6
Craig Dietrich <robyn@Robyns-MacBook-Air.local>
2014-10-13 19:43:44 -0700

    Sample spreadsheet styling

--- application/views/workbench/spreadsheet.php
+++ application/views/workbench/spreadsheet.php
@@ -4,41 +4,41 @@
     
 <table class="table table-hover table-condensed">
 <thead>
-<tr><th>rdf:resource</th><th>dcterms:title</th><th>dcterms:description</th><th>dcterms:source</th><th>dcterms:subject</th><th><big>+</big></th></tr>
+<tr><th>rdf:resource</th><th>dcterms:title</th><th>dcterms:description</th><th>dcterms:source</th><th>dcterms:subject</th><th><big>+/-</big></th></tr>
 </thead>
 <tbody>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
-<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
+<tr><td><a href="#">foobar.jpg</a></td><td>Foobar in Paradise</td><td>Lorem ipsum dolor sit amet, consectetur adipiscing elit ...</td><td>YouTube</td><td>Love, Hate</td><td>&#8615;</td></tr>
 </tbody>
 </table>    
     
--- application/views/workbench/wrapper.php
+++ application/views/workbench/wrapper.php
@@ -8,6 +8,10 @@
 html, body, .container-fluid, .row-fluid, .row, .col-max-height {height:100%;}
 body {font-size:12px;}
 .table-hover {cursor:pointer;}
+thead tr th:last-child {text-align:center;color:#888888;}
+tbody tr td:last-child {text-align:center;color:#888888;}
+th {border-left:solid 1px #dddddd;}
+th:first-of-type {border-left:inherit;}
 </style>
 <?php echo $scripts_header ?>
 </head>
