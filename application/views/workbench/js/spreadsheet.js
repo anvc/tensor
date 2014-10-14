b22476b8b36e53cb0488b2acf3012aaaa5f1efd1
Craig Dietrich <craigdietrich@gmail.com>
2014-10-14 16:53:08 -0700

    Metadata box

--- application/views/workbench/js/spreadsheet.js
+++ application/views/workbench/js/spreadsheet.js
@@ -3,8 +3,8 @@
 		return this.each(function() {
 			var $this = $(this);
 			var width = $this.width();
-			var text_wrapper = $this.find(':first');
-			if (!text_wrapper.is('div, span, p')) return;
+			var text_wrapper = $this.children(':first');
+			if (!text_wrapper.is('div, span, p') || text_wrapper.hasClass('container')) return;
 			text_wrapper.css('width','');
 			$this.find('.more_text').remove();
 			if (parseInt(text_wrapper.outerWidth()) <= parseInt(width)) return;
@@ -12,6 +12,40 @@
 			if (!$this.find('.more_text').length) $this.append('<span class="more_text" style="float:right;">...</span>');
 		});
 	};
+	$.fn.metadataPanel = function() {
+		var $parent = $(this);
+		// Remove
+		if ($parent.hasClass('active')) {
+			$metadata = $parent.next();
+			if (!$metadata.find('.metadata').length) return;
+			$parent.removeClass('active');
+			$metadata.fadeOut(function() {
+				$(this).remove();
+			});
+		// Add
+		} else {
+			$parent.addClass('active');
+			var $metadata = $('<tr style="display:none;"><td colspan="20" class="metadata"><div class="container" style="width:100%;"></div><br clear="both" /></td></tr>');
+			var $container = $metadata.find('.container');
+			$('<div class="row"><div class="col-md-12 col-xs-12">All metadata for this item</div></div>').appendTo($container);
+			// The following will be replaced by a crawl through the actual RDF-JSON node for the row ... for now, fill in placeholders
+			var predicates = [];
+			var $values = $('.spreadsheet tbody td:not(:last)');
+			$('.spreadsheet thead th:not(:last)').each(function() {
+				predicates.push($(this).text());
+			});
+			for (var k = 0; k < predicates.length; k++) {
+				var value = $values.eq(k).children(':first').html();
+				var $row = $('<div class="row"><div class="col-md-3 col-xs-3" style="text-align:left;font-weight:bold;">'+predicates[k]+'</div><div class="col-md-9 col-xs-9" style="text-align:left;">'+value+'</div></div>');
+				$container.append($row);
+			}
+			$('<div class="row"><div class="col-md-3 col-xs-3" style="text-align:left;font-weight:bold;">dcterms:source</div><div class="col-md-9 col-xs-9" style="text-align:left;">YouTube</div></div>').appendTo($container);
+			$('<div class="row"><div class="col-md-3 col-xs-3" style="text-align:left;font-weight:bold;">dcterms:publisher</div><div class="col-md-9 col-xs-9" style="text-align:left;">Internet Movie Database</div></div>').appendTo($container);
+			$('<div class="row"><div class="col-md-3 col-xs-3" style="text-align:left;font-weight:bold;">dcterms:identifier</div><div class="col-md-9 col-xs-9" style="text-align:left;">'+(Math.floor(Math.random()*90000) + 10000)+'</div></div>').appendTo($container);
+			$parent.after($metadata);
+			$metadata.fadeIn();
+		}
+	};	
 }( jQuery ));
 
 $(document).ready(function() {
@@ -22,6 +56,13 @@ $(document).ready(function() {
   
 	$('td').resize(function() {
 		$(this).condenseCellText();
+	});
+	
+	$('tr').on('click', 'td:not(:first)', function() {
+		var $this = $(this);
+		if ($this.hasClass('metadata')) return;
+		var $tr = $this.closest('tr');
+		$tr.metadataPanel();
 	});
 	
 });
