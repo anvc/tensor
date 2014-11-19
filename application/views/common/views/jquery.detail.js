8abcf726dedecd3142a3ea40e83f383e2d95116b
Craig Dietrich <craigdietrich@gmail.com>
2014-11-19 11:09:16 -0800

    Details ("Metadata") view

--- application/views/common/views/jquery.detail.js
+++ application/views/common/views/jquery.detail.js
@@ -29,7 +29,7 @@
     	opts = $.extend( {}, defaults, options );
     	namespaces_reversed();
     	$self = this;
-        do_create_tiles();
+        do_create_details();
         return $self;
     };
     
@@ -50,26 +50,41 @@
     	});    	
     }
     
-    function do_create_tiles() {
+    function do_create_details() {
     	$self.empty();
-		predicates = [
-		                  'http://simile.mit.edu/2003/10/ontologies/artstor#thumbnail',
-		                  'http://purl.org/dc/terms/title',
-		                  'http://purl.org/dc/terms/description'
-		                 ];
-		$self.append('<p>Craig was here</p>');
-    }
-    
-    function do_match_height() {
-    	$self.find('.tile').matchHeight(true);
-    }
-    
-    function obj_length(obj) {
-    	var size = 0, key;
-    	for (key in obj) {
-    		if (obj.hasOwnProperty(key)) size++;
+    	var $wrapper = $('<div class="container-fluid details"></div>').appendTo($self);
+    	for (var j in opts.rows) {
+    		var row = opts.rows[j];
+    		var thumb = ('undefined'!=typeof(row['http://simile.mit.edu/2003/10/ontologies/artstor#thumbnail'])) ? row['http://simile.mit.edu/2003/10/ontologies/artstor#thumbnail'][0].value : $('link#base_url').attr('href')+'application/views/common/views/images/missing_thumb.jpg';
+    		var $row = $('<div class="row"></div>').appendTo($wrapper);
+    		var $thumb = $('<div class="thumb col-lg-3 col-md-3 col-sm-3 col-xs-3"><img src="'+thumb+'" /></div>').appendTo($row);
+    		$checkbox = $('<input type="checkbox" value="'+j+'" />').appendTo($thumb);
+    		if (-1!=opts.check.indexOf(j)) {
+    			$checkbox.prop('checked', true).closest('.row').addClass('details_row_checked');
+    		}    		
+    		var $content = $('<div class="content col-lg-9 col-md-9 col-sm-9 col-xs-9"><div class="container-fluid"></div></div>').appendTo($row);
+    		$('<br clear="both" />').appendTo($row);
+    		var $p = $content.find('.container-fluid');
+    		row = sort_predicates_by_prop(row);
+    		for (var p in row) {
+    			var o = [];
+    			for (k = 0; k < row[p].length; k++) {
+    				o.push(row[p][k].value.linkify());
+    			}
+    			$uri = $('<div class="row"></div>').appendTo($p);
+    			$('<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><b>'+pnode(p)+'</b></div>').appendTo($uri);
+    			$('<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">'+o.join('<br />')+'</div>').appendTo($uri);
+    		}
     	}
-    	return size;	
+    	$self.find('.details .thumb').click(function() {
+    		var is_checked = ($(this).find('input:checked').length) ? true : false;
+    		$(this).find('input[type="checkbox"]').prop('checked',((is_checked)?false:true));
+    		if (is_checked) {
+    			$(this).closest('.row').removeClass('details_row_checked');
+    		} else {
+    			$(this).closest('.row').addClass('details_row_checked');
+    		}
+    	});    	
     }
     
     function pnode(str) {
@@ -80,18 +95,26 @@
     	}
     }
     
-    // http://phpjs.org/functions/basename/
-    function basename(path, suffix) {
-    	var b = path;
-    	var lastChar = b.charAt(b.length - 1);
-    	if (lastChar === '/' || lastChar === '\\') {
-    		b = b.slice(0, -1);
+    function sort_predicates_by_prop(obj) {
+        ps = [];
+        for (var p in obj) {
+        	ps.push(pnode(p).toLowerCase());
     	}
-    	b = b.replace(/^.*[\/\\]/g, '');
-    	if (typeof suffix === 'string' && b.substr(b.length - suffix.length) == suffix) {
-    		b = b.substr(0, b.length - suffix.length);
-    	}
-    	return b;
+        ps.sort();
+    	
+        var results = {};
+        for (var j = 0; j < ps.length; j++) {
+        	p = ps[j];
+        	for (var key in obj) {
+        		if (pnode(key).toLowerCase() == p) {
+        			results[key] = obj[key];
+        			continue;
+        		}
+        	}
+        }
+        
+        return results;
+
     }
     
 }( jQuery ));
