b152770a09850f1335e8937a0919a10caf4102e1
Craig Dietrich <craigdietrich@gmail.com>
2015-03-02 11:59:48 -0800

    proxy model formatting

--- application/models/proxy_model.php
+++ application/models/proxy_model.php
@@ -10,21 +10,21 @@ class Proxy_model extends CI_Model {
     public function get() {
     	
     	$graph_uri =@ $_REQUEST['graph_uri'];
-		$store_uri =@ $_REQUEST['store_uri'];
-		$mapping_uri =@ $_REQUEST['mapping_uri'];
-		$source_uri =@ $_REQUEST['source_uri'];
-		$content_type = (isset($_REQUEST['content_type']) && !empty($_REQUEST['content_type'])) ? $_REQUEST['content_type'] : 'xml';
-		$parser =@ $_REQUEST['parser'];
+	$store_uri =@ $_REQUEST['store_uri'];
+	$mapping_uri =@ $_REQUEST['mapping_uri'];
+	$source_uri =@ $_REQUEST['source_uri'];
+	$content_type = (isset($_REQUEST['content_type']) && !empty($_REQUEST['content_type'])) ? $_REQUEST['content_type'] : 'xml';
+	$parser =@ $_REQUEST['parser'];
 		
-		if (empty($source_uri)) return self::error('Missing source URI');
-		if (empty($parser)) return self::error('Missing parser');
+	if (empty($source_uri)) return self::error('Missing source URI');
+	if (empty($parser)) return self::error('Missing parser');
 		
-		$parser_path = FCPATH."/application/views/ui/parsers/$parser.php";
-		if (!file_exists($parser_path)) return self::error('Could not find parser PHP file');
-		require_once($parser_path);
-		if (empty($content)) return self::error('Could not resolve content');
+	$parser_path = FCPATH."/application/views/ui/parsers/$parser.php";
+	if (!file_exists($parser_path)) return self::error('Could not find parser PHP file');
+	require_once($parser_path);
+	if (empty($content)) return self::error('Could not resolve content');
 		
-		return $content;
+	return $content;
     	
     }
     
@@ -36,7 +36,6 @@ class Proxy_model extends CI_Model {
     
     private function ttl_to_rdf($content='') {
 
-    	
     }
     
 }
